436bc8404f4aefc5ab89a45ae23af6fef372631b
666reddragon <prekshappaul@gmail.com>
2026-05-04 17:44:39 -0400

    My first Homework form BroCode

---
+++ Homework.py
@@ -0,0 +1,18 @@
+#Strings
+user_name = "Bro Code"
+print(f"My username is {user_name}")
+#Integers
+year = 2024
+
+print(f"The year I am typing this code is {year}")
+#Float
+pi = 3.14
+
+print(f"The value of pi is {pi}")
+#Boolean
+is_admin = True
+
+if is_admin:
+    print("You are admin")
+else:
+    print("You are NOT admin")
