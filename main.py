1672138b8a6c731d416db65a28d39eeec6f57382
666reddragon <94418835+666reddragon@users.noreply.github.com>
2026-05-04 17:22:09 -0400

    added python basics code

---
+++ main.py
@@ -0,0 +1,51 @@
+# Strings
+first_name = "Preksha"
+food = "pizza"
+email = "preksha@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+
+#Integers
+age = 23
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+
+#Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}")
+print (f"Your gpa is: {gpa}")
+print(f"You ran {distance}km")
+
+
+#Boolean
+is_student = False
+for_sale = False
+is_online = False
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
+
+if for_sale:
+    print("That item is for sale")
+else:
+    print("That item is NOT available")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
+
+
