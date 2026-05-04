a5ce88a36434c5e1e171f0f968c0c9fd09204f56
666reddragon <prekshappaul@gmail.com>
2026-05-04 19:28:35 -0400

    added user input exercises and practice

---
+++ main3_ user input.py
@@ -0,0 +1,34 @@
+#input()
+
+#name = input("What is your name?: ")
+#age = input("How old are you?: ")
+#age = int(age)
+#age = age + 1
+
+#print(f"Hello {name}!")
+#print("HAPPY BIRTHDAY!")
+#print(f"You are {age} years old")
+
+#Excersice 1 Rectangle Area Calc
+
+#length = float(input("Enter the length: "))
+#width = float(input("Enter the width: "))
+#area = length * width
+
+#print(f"The area is: {area}cm²")
+
+
+#Exercise 2 Shopping Cart Program
+
+item = input("What item would ")
+
+
+
+
+
+
+
+
+
+
+
